dcd693042f3a19488109c53e7401efa3e4781430
Catarina Teixeira <rtd1001@alu.ubu.es>
2022-07-04 00:21:53 +0200

    implementacion avisos #56

--- RepresentacionGraficaMatricula/src/app/filtros/filtro2/filtro2.component.ts
+++ RepresentacionGraficaMatricula/src/app/filtros/filtro2/filtro2.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ChangeDetectorRef, Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Series } from 'src/app/series';
 import { XlsDataService } from 'src/app/services/xls-data.service';
 
@@ -12,7 +12,7 @@ export class Filtro2Component implements OnInit {
   dynamicSeries: Array<Series> = [];
   dynamicAux: any = {};
   xlsData: any[];
-
+  
   degreesList = {}
   yearsList = {}
   docsList = []
@@ -24,7 +24,8 @@ export class Filtro2Component implements OnInit {
   @Output()borraGrafica:EventEmitter<number>=new EventEmitter();
 
   constructor(
-    private _xlsData: XlsDataService
+    private _xlsData: XlsDataService,
+    private cdref: ChangeDetectorRef
   ) { }
 
   ngOnInit(): void {
@@ -35,8 +36,12 @@ export class Filtro2Component implements OnInit {
     //Obtengo el excel y saco el listado de documentos
     this.xlsData = this._xlsData.getXlsData();
     this.docsList = this.getDocs();
+
   }
 
+  ngAfterViewInit(){
+    this.cdref.detectChanges();
+  }
   /**
      * Obtiene el listado de documentos
      * @returns Listado de documentos según los excel subidos
@@ -75,53 +80,76 @@ changeDoc(value, i) {
   
 }
 
-changeDegree(value, i) {
+changeDegree(select, i) {
   this.borraGrafica.emit(1);
-  this.selectedDegree = value;
-  this.dynamicSeries[i].degrees = this.selectedDegree;
-
-  this.resetYear(i);
- // this.resetSemester(i);
-
-  var info = this.xlsData[this.selectedDoc];
-
-  var data = info[this.selectedDegree].data;
-
-  var yearDuplicates = []
-  Object.keys(data).forEach(key => {
-      yearDuplicates.push(data[key].asig_curso);
-  });
-
-  var year = Array.from(new Set(yearDuplicates))
-  var listAux = [];
-  for (var z = 0; z < year.length; z++) {
-      listAux.push(year[z]);
+  let error;
+  let value = select.value;
+  for(let serie of this.dynamicSeries){
+    let degree = serie.degrees;
+    if(degree){
+      if(degree != value){
+        error = true;
+        alert("Se debe seleccionar el mismo grado/master.")
+        select.control.reset();
+      }
+    }
   }
-  this.yearsList[i] = listAux;
+  if(!error){
+    this.selectedDegree = value;
+    this.dynamicSeries[i].degrees = this.selectedDegree;
+
+    this.resetYear(i);
+
+    var info = this.xlsData[this.selectedDoc];
+
+    var data = info[this.selectedDegree].data;
+
+    var yearDuplicates = []
+    Object.keys(data).forEach(key => {
+        yearDuplicates.push(data[key].asig_curso);
+    });
+
+    var year = Array.from(new Set(yearDuplicates))
+    var listAux = [];
+    for (var z = 0; z < year.length; z++) {
+        listAux.push(year[z]);
+    }
+    this.yearsList[i] = listAux;
+  }
 }
 
-changeYear(value, i) {
+changeYear(select, i) {
 this.borraGrafica.emit(1);
-  this.selectedYear = value;
-  this.dynamicSeries[i].years = this.selectedYear;
-
- // this.resetSemester(i);
-
-  var info = this.xlsData[this.selectedDoc];
-
-  var selectedInfo = info[this.selectedDegree].data.filter(row => (row.asig_curso == this.selectedYear));;
-
-  var semesterDuplicates = []
-  Object.keys(selectedInfo).forEach(key => {
-      semesterDuplicates.push(selectedInfo[key].asig_vp);
-  });
-  var semester = Array.from(new Set(semesterDuplicates))
-  var listAux = []
-  for (var z = 0; z < semester.length; z++) {
-      listAux.push(semester[z]);
+  let error;
+  let value = select.value;
+  for(let serie of this.dynamicSeries){
+    let year = serie.years;
+    if(year){
+      if(year != value){
+        error = true;
+        alert("Se debe seleccionar el mismo curso.")
+        select.control.reset();
+      }
+    }
   }
-//  this.semestersList[i] = listAux;
+  if(!error){
+    this.selectedYear = value;
+    this.dynamicSeries[i].years = this.selectedYear;
 
+    var info = this.xlsData[this.selectedDoc];
+
+    var selectedInfo = info[this.selectedDegree].data.filter(row => (row.asig_curso == this.selectedYear));;
+
+    var semesterDuplicates = []
+    Object.keys(selectedInfo).forEach(key => {
+        semesterDuplicates.push(selectedInfo[key].asig_vp);
+    });
+    var semester = Array.from(new Set(semesterDuplicates))
+    var listAux = []
+    for (var z = 0; z < semester.length; z++) {
+        listAux.push(semester[z]);
+    }
+  }
 }
 
 resetDegree(i) {
@@ -148,72 +176,7 @@ makeData(): any {
     } else{
 
       var dataSerie = [];
-      console.log(this.dynamicSeries.length)
-      /*
-      for (var i = 0; i < this.dynamicSeries.length; i++) {
-          var descriptionArray = [];
-
-          var data = this.xlsData[this.dynamicSeries[i].docs];
-          var info = data[this.dynamicSeries[i].degrees].data.filter(row => (
-              row.asig_curso == this.dynamicSeries[i].years
-              && row.asig_tipAcademica == 'Teoría'
-              && row.asig_activ == 'S'));
-              console.log('info')   
-          console.log(info)
-          var groupsDuplicates = [];
-          var descriptionDuplicates = [];
-          Object.keys(info).forEach(key => {
-              groupsDuplicates.push(info[key].asig_grupo);
-              descriptionDuplicates.push(info[key].asig_descripcion);
-          });
-
-          var groups = Array.from(new Set(groupsDuplicates));
-          var description = Array.from(new Set(descriptionDuplicates));
-          descriptionArray.push(description);
-          console.log('groups')
-          console.log(groups)
-          console.log('description')
-          console.log(description)
-          var descpInfo = {};
-          var series = []
-          var sumaGrupo = 0;
-
-        for (var z = 0; z < description.length; z++){
-
-          var info_description = info.filter(row => row.asig_descripcion == description[z])
-          console.log(info_description)
-
-          sumaGrupo = 0;
-          for (var j = 0; j < groups.length; j++) {
-
-            var info_group = info_description.filter(row => row.asig_grupo == groups[j])
-            console.log(info_group)
-
-            Object.keys(info_group).forEach(key => {
-                sumaGrupo += info_group[key].alum_total;
-
-            });
-          }
-
-
-          var groupInfo = []
-          groupInfo.push({
-            name: this.dynamicSeries[i].docs,
-            value: sumaGrupo
-          })
-
-          series.push(groupInfo);
-
-          dataSerie[z] = {
-            name: description[z],
-            series: groupInfo
-          }
-        }
-
-        
-      }*/
-
-
+      
       for (var i = 0; i < this.dynamicSeries.length; i++) {
         var descriptionArray = [];
 
@@ -275,72 +238,11 @@ makeData(): any {
             }
           }
           
-          
-
-          /*    var groupInfo = {}
-          groupInfo = {
-            name: this.dynamicSeries[i].docs,
-            value: sumaGrupo
-          }*/
-          /*
-          var groupInfo = []
-          groupInfo.push({
-            name: this.dynamicSeries[i].docs,
-            value: sumaGrupo
-          })
-          */
-        
-           //  series[z] = groupInfo;
-          /*   dataSerie[z] = {
-            name: description[z],
-            series: series
-          }*/
-          /*
-          if (i = 0){
-            dataSerie[z] = {
-              name: description[z],
-              series: groupInfo
-            }
-          }else{
-            dataSerie[z] = {
-              name: description[z],
-              series: series.push(groupInfo)
-            }
-          }
-          */
-        
         }
-
 
       }
 
-
-
-
-      /*
-        for (var w = 0; w < description.length; w++){
-
-
-          dataSerie[w] = {
-            name: description[w],
-            series: groupInfo[w]
-          }
-          
-        }*/
-        console.log('dataSerie')
-        console.log(dataSerie)
-        /*
-        dataSerie[i] = {
-            x: this.selectedDegree + '-' + this.selectedYear + '-' + this.selectedSemester,
-            y: average,
-            maxY: maxAverage,
-        //    chartInfo: groupInfo,
-            des: descriptionArray,
-            maxY_group: maxGroup
-        }
-      */
-        return dataSerie;
-
+      return dataSerie;
 
     }//else
   }//makeData
